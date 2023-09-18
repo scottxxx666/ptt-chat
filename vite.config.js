55a4bff09b1c322244afc1c24748d01ff368f85f
Scott Kao <scottxxx666@gmail.com>
2023-09-18 16:48:56 +0800

    chore: enable eslint while running

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,17 @@ import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import {crx} from '@crxjs/vite-plugin'
 import manifest from './manifest.json'
+import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), crx({manifest})],
+  plugins: [
+    react(),
+    crx({manifest}),
+    checker({
+      eslint: {
+        lintCommand: 'eslint "./src/**/*.{js,jsx}"',
+      },
+    }),
+  ],
 })
